677b94da090cef70eff49a8da637be272fddc3c6
Ivan Willig <iwillig@gmail.com>
2019-12-06 14:22:14 -0500

    Add basic app that allows the user to select three attack cards

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
+import Constants from 'expo-constants';
 
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'Quick Attack',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Hard Attack',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Precise Attack',
+  },
+];
+
+function Item({ id, title, selected, onSelect }) {
+  return (
+      <TouchableOpacity
+          onPress={() => onSelect(id)}
+          style={[
+            styles.item,
+            { backgroundColor: selected ? '#6e3b6e' : '#f9c2ff' },
+          ]}
+      >
+        <Text style={styles.title}>{title}</Text>
+      </TouchableOpacity>
+  );
+}
 
 export default function App() {
+  const [selected, setSelected] = React.useState(new Map());
+
+  const onSelect = React.useCallback(
+      id => {
+        const newSelected = new Map(selected);
+        newSelected.set(id, !selected.get(id));
+
+        setSelected(newSelected);
+      },
+      [selected],
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+            data={DATA}
+            renderItem={({ item }) => (
+                <Item
+                    id={item.id}
+                    title={item.title}
+                    selected={!!selected.get(item.id)}
+                    onSelect={onSelect}
+                />
+            )}
+            keyExtractor={item => item.id}
+            extraData={selected}
+        />
+      </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
   },
 });
